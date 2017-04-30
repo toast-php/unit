2642c782a8f630ef4d9fd4153414e652a4de68b3
Marijn Ophorst <marijn@monomelodies.nl>
2017-04-30 10:05:09 +0200

    add demo class

---
+++ demo/Test.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Toast\Demo;
+
+class Test
+{
+    public $foo = 'bar';
+
+    public function test()
+    {
+        echo '    ';
+        return true;
+    }
+
+    public static function aStaticMethod()
+    {
+        return true;
+    }
+}
+

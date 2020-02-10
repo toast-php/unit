c8c01c4b4ed9053a565ebc6a0f24eaea376f3702
Marijn Ophorst <marijn@monomelodies.nl>
2020-02-10 03:00:26 +0100

    bump

--- src/Command.php
+++ src/Command.php
@@ -22,7 +22,7 @@ class Command extends Cliff\Command
 {
     use OutputHelper;
 
-    const TOAST_VERSION = '2.0.2';
+    const TOAST_VERSION = '2.1.2';
 
     /** @var bool */
     public $output = false;
